49f6360e0c1ab3d2ab43b05904a02d5c2b2c344e
tammarut <tammarut.nua@gmail.com>
2019-03-22 16:28:13 +0700

    comment explain

--- main.go
+++ main.go
@@ -8,39 +8,52 @@ import (
 )
 
 func main() {
+	//=>ชื่อfile config.yaml
 	viper.SetConfigName("config")
+
+	//=>Path
 	viper.AddConfigPath(".")
+
+	//=>อ่านvalue จากENV variable
 	viper.AutomaticEnv()
 
+	//=> แทน _ ด้วย . ในenv
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
+	//=> read config
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 
+	//.Viper Method
 	fmt.Println(viper.Get("app.env"))
 	fmt.Println(viper.Get("app.port"))
 	fmt.Println(viper.Get("app.debug"))
 	fmt.Println("=========================")
 
+	//=>Get as "string"
 	fmt.Println(viper.GetString("mongodb.connection"))
 	fmt.Printf("%T \n", viper.GetString("mongodb.connection"))
 	fmt.Println("=========================")
 
+	//=>Get as bool(true/false)
 	fmt.Println(viper.GetBool("app.debug"))
 	fmt.Println("=========================")
 
+	//=>Return bool
 	if viper.IsSet("app.debug") {
 		fmt.Println("Yep! debug")
 	}
 	fmt.Println("=========================")
 
+	//=>String Map
 	log := viper.GetStringMap("log")
 	fmt.Println(log["level"])
 	fmt.Println(log["format"])
 	fmt.Println("=========================")
 
+	//=>Set default env
 	viper.SetDefault("app.env", "production")
 	fmt.Println(viper.Get("app.env"))
 
